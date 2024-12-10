58aed462d4d74ce5201415b8648f6da012673aa1
webNekit <mochalov-hunter@mail.ru>
2024-12-10 13:34:45 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 import { toggleNavbar } from "./modules/toggler-navbar.js";
+import { initSlider } from "./modules/reviews-slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     lucide.createIcons();
     toggleNavbar();
+    initSlider();
 })
