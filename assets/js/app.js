bf7728af5434d001683982cbd03cf4cf2bdca7d2
webNekit <mochalov-hunter@mail.ru>
2024-12-10 15:06:12 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 import { toggleNavbar } from "./modules/toggler-navbar.js";
 import { initSlider } from "./modules/reviews-slider.js";
+import { initPhoneMask } from "./modules/phone-mask.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     lucide.createIcons();
+    initPhoneMask();
     toggleNavbar();
     initSlider();
 })
